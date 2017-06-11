63163aeba6b0bb2dd0ee82c60b192af3139280aa
SummerGlow <1494381494@qq.com>
2017-06-11 16:20:30 +0800

    wangye.js

---
+++ wangye.js
@@ -0,0 +1,12 @@
+	function done(){
+		var Mimi1,Mimi2,Mimi3;
+		Mimi1=document.forms[0].zhanghao.value;
+		Mimi2=document.forms[0].mima.value;
+		Mimi3=Mimi1+Mimi2;
+		if(Mimi3==="xiahui66666"){
+			open("rose.html","","");
+		}
+		if(Mimi3==="xiahui88888"){
+			open("ggfly.html","","")
+		}
+	}
